559c6b2bfd0b7f1545c15de2e2a897b8629d38f1
SuigintouS5 <ptdidoanhtrai@gmail.com>
2026-01-22 15:10:07 +0700

    Delete Django directory

--- Django/doctruyen/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DoctruyenConfig(AppConfig):
-    name = 'doctruyen'
--- Django/doctruyen/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.truyen_list, name='truyen-list'),
-    path('truyen/<slug:slug>/', views.truyen_detail, name='truyen-detail'),
-]
