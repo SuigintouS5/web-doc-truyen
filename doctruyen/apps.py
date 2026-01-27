bddc0d97365a7b5325ebc6affdae8148d0639d55
SuigintouS5 <ptdidoanhtrai@gmail.com>
2026-01-27 21:00:34 +0700

    Delete doctruyen directory

--- doctruyen/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class DoctruyenConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'doctruyen'
-    
-    def ready(self):
-        import doctruyen.signals
